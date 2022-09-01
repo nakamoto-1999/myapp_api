ee6b6c3bbb5c8cf02dee0a00fef5677d1a6cd8e2
Nakano <hirota7771@rakuten.jp>
2022-09-01 11:03:59 +0900

    revise config

--- src/main/java/com/example/admin/WebMvcConfig.java
+++ src/main/java/com/example/admin/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(crosOrigin)
                 .allowedMethods("GET" , "POST" , "PUT" , "DELETE")
                 .allowCredentials(true);
     }
