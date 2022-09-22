0ffd1758f6785a9a92c4bf4d23c68799142d1c5a
Nakano <hirota7771@rakuten.jp>
2022-09-22 15:13:25 +0900

    revise cros re

--- src/main/java/com/example/admin/WebMvcConfig.java
+++ src/main/java/com/example/admin/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://" + crosOrigin , "https://" + crosOrigin)
+                .allowedOrigins(crosOrigin)
                 .allowedMethods("GET" , "POST" , "PUT" , "DELETE")
                 .allowCredentials(true);
 
